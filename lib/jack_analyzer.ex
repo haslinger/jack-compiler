33d20c6e584c6f6df1b60998b39cd917bcab8699
stefan Haslinger <mail@stefan-haslinger.at>
2021-03-08 11:55:01 +0100

    comment

--- lib/jack_analyzer.ex
+++ lib/jack_analyzer.ex
@@ -2,6 +2,9 @@ defmodule JackAnalyzer do
   def main(args) do
     { _, [directory | _], _ } = OptionParser.parse(args, strict: [])
 
+    # Create output directory, if it doesn't exist
+    File.mkdir_p!("../" <> directory <> "/output")
+
     "../" <> directory <> "/*.jack"
     |> Path.wildcard()
     |> Enum.each(&process(&1))
