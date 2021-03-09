02cbfb302461634c2cfe192d0dce6d79558d5c24
stefan Haslinger <mail@stefan-haslinger.at>
2021-03-09 15:15:05 +0100

    add debugging info

--- lib/jack_analyzer.ex
+++ lib/jack_analyzer.ex
@@ -11,6 +11,7 @@ defmodule JackAnalyzer do
   end
 
   def process(file, directory) do
+    IO.puts ("=== Processing:" <> file <> "===")
     {:ok, body} = File.read(file)
 
     filename = file |> Path.basename() |> String.split(".") |> List.first()
